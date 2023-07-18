d0a3c6199ffae7576a150571f0979f1ca3a38209
eyalg <narnavg@gmail.com>
2023-07-18 12:14:01 +0300

    add MUI

--- src/Waga.tsx
+++ src/Waga.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { MyContext } from "./MyContext";
 import { MyLogin } from './MyLogin'
-
+import Button from '@mui/material/Button';
 const Waga = () => {
   const { someColor, cngColor } = useContext(MyContext);
   const { logged,cngLog } = useContext(MyLogin);
@@ -9,9 +9,10 @@ const Waga = () => {
     <div>
       Waga
       <div>
-      <button  disabled={!logged} onClick={() => cngColor("green")}> Green </button>
-      <button  disabled={!logged} onClick={() => cngColor("red")}> red </button>
-      <button  disabled={!logged} onClick={() => cngColor("yellow")}> yellow </button>
+      <Button disabled={!logged} variant="contained" color="success" onClick={() => cngColor("gray")}>Gray</Button>
+      <Button  disabled={!logged} variant="outlined" color="secondary" onClick={() => cngColor("green")}> Green </Button>
+      <Button  disabled={!logged} variant="text" onClick={() => cngColor("red")}> red </Button>
+      <Button  disabled={!logged} variant="contained" onClick={() => cngColor("yellow")}> yellow </Button>
       </div>
     </div>
   );
